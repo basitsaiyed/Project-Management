0ca7e200118e58f41ec7a6c43da156424a731dfb
Basit <basitsaiyed19123@gmail.com>
2024-11-14 09:12:46 +0530

    Setup Navbar and Sidebar on frontend

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex flex-col min-h-screen justify-between items-center p-24">
+        hello
+      </main>
+  );
+}
